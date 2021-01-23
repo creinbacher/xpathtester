155ff96e2097953bf30cdce2f95414ed1e53b51e
Christopher Reinbacher <reinbacher@gmail.com>
2021-01-23 11:36:02 +0100

    added svelte

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,38 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { SidebarProvider } from "./SidebarProvider";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "xpathtester" is now active!');
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      "xpathtester-sidebar",
+      sidebarProvider
+    )
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('xpathtester.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "xpathtester" is now active!');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from xpath_tester!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "xpathtester.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
 
-	context.subscriptions.push(disposable);
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from xpath_tester!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
---
+++ webviews/components/sidebar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<form id="xpath">
+  <label for="expression">X-Path Expression:</label>
+  <input id="expression" name="expression" type="text" />
+
+  <label for="cnode">Context Node:</label>
+  <input id="cnode" name="cnode" type="text" />
+
+  <button type="reset">Reset</button>
+  <button type="submit">Check</button>
+</form>
