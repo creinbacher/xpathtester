a3d0a1b561d30612d71d313a9cfc25f67aab378d
Christopher Reinbacher <reinbacher@gmail.com>
2021-02-28 12:10:12 +0100

    refactored decoration

--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -109,9 +109,16 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
       this.xpathOut.clear();
       queryResult = this.xpathWrapper.checkXPath(query, xml);
       if (!queryResult || queryResult.length === 0) {
+        let out = "Found no results for expression '" + query.expression;
+        out += this.checkForContext(query.contextNode);
+        out += "'";
+        this.xpathOut.appendLine(out);
         return;
       }
-      if (queryResult.length > 0 && queryResult[0].numericResult) {
+      if (
+        queryResult.length > 0 &&
+        undefined !== queryResult[0].numericResult
+      ) {
         let sumOfResults: number = 0;
         if (queryResult.length > 1) {
           //sum up all the numeric results
@@ -174,7 +181,7 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
       this.xpathOut,
       activeTextEditor
     );
-    const xpathResults: vscode.DecorationOptions[] = decorationProcesser.collectDecorations(
+    const xpathResults: vscode.DecorationOptions[] = decorationProcesser.collectDecorationsV2(
       queryResult
     );
 
